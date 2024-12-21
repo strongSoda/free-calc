e5c6e8ecb719d3d187556c9ed8d781fe95fcab7e
strongSoda <strongsoda2@gmail.com>
2024-12-21 16:40:46 +0530

    ad fix

--- src/components/AdUnit.jsx
+++ src/components/AdUnit.jsx
@@ -66,7 +66,7 @@ const AdUnit = ({ type = 'square' }) => {
   };
 
   const config = adConfigs[type];
-  const isDevelopment = true;
+  const isDevelopment = false;
 
   // Set isClient on mount
   useEffect(() => {
